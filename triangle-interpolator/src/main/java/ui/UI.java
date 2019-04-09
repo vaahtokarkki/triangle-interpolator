074e11cec5a2f2ba8612b85ddef3bfd132d3bbe1
Roni Lindholm <roni.lindholm@gmail.com>
2019-04-09 15:33:46 +0300

    Käyttöliittymää tehty

--- triangle-interpolator/src/main/java/ui/UI.java
+++ triangle-interpolator/src/main/java/ui/UI.java
@@ -17,7 +17,13 @@ import utils.MyMath;
 public class UI {
 
     private Scanner sc;
+    CsvParse parser;
+
     private String fileName;
+    private String csvSeparator;
+
+    private int XCoord, YCoord, ZCoord;
+
     private int width;
     private int height;
 
@@ -31,6 +37,10 @@ public class UI {
 
     public void start() {
         selectFile();
+        selectHeadersForFile();
+        selectDimensions();
+        System.out.println("*****************************");
+        printFileStats();
         runInterpolation();
     }
 
@@ -47,14 +57,13 @@ public class UI {
 
         printFiles(listOfFiles);
 
-        int fileId = readNumber();
-        while (fileId >= listOfFiles.length) {
-            System.out.println("File number must be between 1-" + listOfFiles.length);
-            fileId = readNumber();
-        }
+        int fileId = readNumber(1, listOfFiles.length);
 
         fileName = listOfFiles[fileId - 1].getName();
-        System.out.println(fileName);
+
+        getCsvSeparator();
+
+        parser = new CsvParse(fileName);
     }
 
     private void printFiles(File[] files) {
@@ -63,11 +72,39 @@ public class UI {
         }
     }
 
-    private int readNumber() {
+    private int readNumber(int min, int max, int defaultValue) {
+        int output;
+        while (true) {
+
+            String line = sc.nextLine();
+            line = line.trim();
+            if (line.isEmpty()) {
+                return defaultValue;
+            }
+
+            try {
+                output = Integer.parseInt(line);
+                if (output < min || output > max) {
+                    System.out.println("Number must be in range " + min + "-" + max);
+                    continue;
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println("Please give a number");
+            }
+        }
+        return output;
+    }
+
+    private int readNumber(int min, int max) {
         int output;
         while (true) {
             try {
                 output = Integer.parseInt(sc.nextLine());
+                if (output < min || output > max) {
+                    System.out.println("Number must be in range " + min + "-" + max);
+                    continue;
+                }
                 break;
             } catch (Exception e) {
                 System.out.println("Please give a number");
@@ -78,22 +115,74 @@ public class UI {
 
     private void runInterpolation() {
         CsvParse parser = new CsvParse(fileName);
-        MyArrayList<geometry.Point> list = parser.parsePointsFromFile(";", "x", "y", "weight");
+        MyArrayList<geometry.Point> list = parser.parsePointsFromFile(csvSeparator, XCoord, YCoord, ZCoord);
         list = MyMath.moveCoordinatesToOrigin(list);
         list = MyMath.scaleCoordinates(1000, 1000, list);
 
         System.out.println("Generating Delaunay triangles");
         MyHashSet<Triangle> t = triangulate(list);
-        double[][] barycentricInterpolation = interpolateMatrix(1000, 1000, list, 25);
-        double[][] idwInterpolation = interpolateInverseDistance(1000, 1000, list, 300, 2, 15);
+        double[][] barycentricInterpolation = interpolateMatrix(width, height, list, 25);
+        double[][] idwInterpolation = interpolateInverseDistance(width, height, list, 300, 2, 15);
 
         System.out.println("Writing barycentric");
         writeToGrayscaleImage(barycentricInterpolation, "barycentric_test.jpg");
         System.out.println("Writing idw");
 
         writeToGrayscaleImage(idwInterpolation, "idw_test.jpg");
-        writeTrianglesToImage(1000, 1000, t, "triangles.jpg");
+        writeTrianglesToImage(width, height, t, "triangles.jpg");
         System.out.println("writing triangles");
+    }
+
+    private void selectHeadersForFile() {
+        String[] headers = parser.getCsvHeaders(csvSeparator);
+        printColumns(headers);
+
+        System.out.println("Select column for x-coordinate (1-" + headers.length + ")");
+        XCoord = readNumber(1, headers.length) - 1;
+
+        System.out.println("Select column for y-coordinate (1-" + headers.length + ")");
+        YCoord = readNumber(1, headers.length) - 1;
+
+        System.out.println("Select column for value to interpolate (1-" + headers.length + ")");
+        ZCoord = readNumber(1, headers.length) - 1;
+    }
+
+    private void getCsvSeparator() {
+        System.out.println("Separator used in csv-file? [;]");
+        String separator = sc.nextLine();
+
+        separator = separator.trim();
+
+        if (separator.isEmpty()) {
+            this.csvSeparator = ";";
+            return;
+        }
+
+        while (separator.length() != 1) {
+            System.out.println("Separator must be exactly one character long!");
+            separator = sc.nextLine();
+            separator = separator.trim();
+        }
+        this.csvSeparator = separator;
+    }
+
+    private void printColumns(String[] headers) {
+        String output = "";
+        for (int i = 0; i < headers.length; i++) {
+            output += "[" + (i + 1) + "] " + headers[i] + " ";
+        }
+        System.out.println(output);
+    }
+
+    private void selectDimensions() {
+        System.out.println("Give width of output image in pixels [1000]");
+        width = readNumber(1, 2000, 1000);
+        System.out.println("Give height of output image in pixels [1000]");
+        height = readNumber(1, 2000, 1000);
+    }
+
+    private void printFileStats() {
+        System.out.println("Found from file "+fileName+" "+(parser.rowsInFile()-1)+" rows");
     }
 
 }
--- triangle-interpolator/src/main/java/utils/CsvParse.java
+++ triangle-interpolator/src/main/java/utils/CsvParse.java
@@ -14,6 +14,7 @@ import java.util.logging.Logger;
 public class CsvParse {
 
     private String fileName;
+    private MyArrayList<String> linesFromFile;
 
     /**
      * Creates new csv-parser with given filename to read data from.
@@ -22,10 +23,38 @@ public class CsvParse {
      */
     public CsvParse(String fileName) {
         this.fileName = fileName;
+        linesFromFile = new MyArrayList<>();
     }
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+        linesFromFile = new MyArrayList<>();
+    }
+
+    /**
+     * Returns how many files are in given file, including csv-header row.
+     *
+     * @return amount of rows in file
+     */
+    public int rowsInFile() {
+        if (linesFromFile.size() == 0) {
+            readFile();
+        }
+
+        return linesFromFile.size();
+    }
+
+    private void readFile() {
+        if (fileName.trim().isEmpty()) {
+            return;
+        }
+
+        try {
+            linesFromFile = io.Reader.readRows(fileName);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            return;
+        }
     }
 
     /**
@@ -42,49 +71,38 @@ public class CsvParse {
      * @return {@link utils.MyArrayList} of parsed {@link geometry.Point} or
      * null if invalid file
      */
-    public MyArrayList<Point> parsePointsFromFile(String csvSeparator, String XCoord, String YCoord, String ZValue) {
-        MyArrayList<String> lines;
-        try {
-            lines = io.Reader.readRows(fileName);
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-            return null;
+    public MyArrayList<Point> parsePointsFromFile(String csvSeparator, int XCoord, int YCoord, int ZValue) {
+        if (linesFromFile.size() == 0) {
+            readFile();
         }
 
         //If file contains only header row
-        if (lines.size() <= 1) {
+        if (linesFromFile.size() <= 1) {
             return null;
         }
-        
+
         MyArrayList<Point> output = new MyArrayList<>();
-        MyArrayList<String[]> valuesFromFile = parseValuesFromCSVLines(lines, csvSeparator);
-
-        String[] headers = valuesFromFile.get(0);
-        int[] columnIndexes = mapColumnsForValues(headers, XCoord, YCoord, ZValue);
-
-        if (columnIndexes == null) {
-            return output;
-        }
+        MyArrayList<String[]> valuesFromFile = parseValuesFromCSVLines(csvSeparator);
 
         for (int i = 1; i < valuesFromFile.size(); i++) {
             String[] row = valuesFromFile.get(i);
             double X, Y, Z;
             try {
-                X = Double.parseDouble(row[columnIndexes[0]]);
+                X = Double.parseDouble(row[XCoord]);
             } catch (Exception e) {
                 e.printStackTrace();
                 continue;
             }
 
             try {
-                Y = Double.parseDouble(row[columnIndexes[1]]);
+                Y = Double.parseDouble(row[YCoord]);
             } catch (Exception e) {
                 e.printStackTrace();
                 continue;
             }
 
             try {
-                Z = Double.parseDouble(row[columnIndexes[2]]);
+                Z = Double.parseDouble(row[ZValue]);
             } catch (Exception e) {
                 e.printStackTrace();
                 Z = Double.NaN;
@@ -97,29 +115,47 @@ public class CsvParse {
     }
 
     /**
+     * Returns an array of headers in given csv file. That is the first line of
+     * file, splited by given separator.
+     *
+     * @param csvSeparator csv separator to use in split
+     * @return an array of headers, null if invalid file
+     */
+    public String[] getCsvHeaders(String csvSeparator) {
+        if (linesFromFile.size() == 0) {
+            readFile();
+        }
+
+        if (linesFromFile == null || linesFromFile.size() == 0) {
+            return null;
+        }
+
+        return linesFromFile.get(0).split(csvSeparator);
+    }
+
+    /**
      * Creates a list of arrays with values from single string representing a
      * row in csv file. That is, the strings are splitted with separator and one
      * array contains all values of one row.
      *
      * TODO: don't ignore empty values! "hello;;world" -> [hello,null,world]
      *
-     * @param lines array list of lines from csv file
      * @param csvSeparator separator to use in parsing
      * @return array list containing parsed values in arrays
      */
-    private MyArrayList<String[]> parseValuesFromCSVLines(MyArrayList<String> lines, String csvSeparator) {
+    private MyArrayList<String[]> parseValuesFromCSVLines(String csvSeparator) {
         MyArrayList<String[]> output = new MyArrayList<>();
 
-        if (lines == null) {
+        if (linesFromFile == null) {
             return null;
         }
 
-        if (lines.size() == 0) {
+        if (linesFromFile.size() == 0) {
             return output;
         }
 
-        for (int i = 0; i < lines.size(); i++) {
-            String line = lines.get(i);
+        for (int i = 0; i < linesFromFile.size(); i++) {
+            String line = linesFromFile.get(i);
             output.add(line.split(csvSeparator));
         }
 
@@ -131,6 +167,8 @@ public class CsvParse {
      * row of csv file. Other columns are ignored. Output is an array containing
      * in 1st element index of X-coordinate, in 2nd element index of
      * Y-coordinate and 3rd element containing index of Z-coordinate
+     *
+     * Note: Not used anymore!
      *
      * @param headerRow array of values in header row
      * @param XCoord name of X-coordinate column
